7e5730fe10a57bdd4b02537b587923e49f423fd7
rowlet <kagechi@naver.com>
2022-11-30 07:58:24 +0900

    버전변경 관련 파일 수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
